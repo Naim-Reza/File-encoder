31d17d0e7f26d08152449f0aeb3fb833e6c188ca
Naim Reza <naim.reza@cellosco.pe>
2021-10-05 11:33:56 +0600

    write initial test code

--- src/reza/encoder/FileEncoder.java
+++ src/reza/encoder/FileEncoder.java
@@ -1,5 +1,22 @@
 package reza.encoder;
 
-public class FileEncoder {
+import java.io.File;
 
+public class FileEncoder {
+    private static String portSeparator = ":";
+
+    public static void main(String[] args) {
+
+        try{
+            File file = new File(args[0]);
+            String encodingMethod = args[1];
+            String[] serverInfo = args[2].split(portSeparator);
+
+            //read and encode the file
+            
+
+        } catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+    }
 }
